4c48a8d15cdf9e958328259d54d80f883a055d5b
Pierrick Koch <pierrick.koch@gmail.com>
2012-03-26 11:49:55 +0700

    replace pygame by gstreamer

--- dynamicsound.py
+++ dynamicsound.py
@@ -9,17 +9,20 @@ http://opensource.org/licenses/BSD-3-Clause
 http://pierriko.com/dynamicsound/
 """
 
+import os
 import sys
 import array
+import urllib
+
 try:
     import cv
 except ImportError:
     print("install opencv: sudo apt-get install python-opencv")
     sys.exit(1)
 try:
-    import pygame
+    import gst
 except ImportError:
-    print("install pygame: sudo apt-get install python-pygame")
+    print("install gstream: sudo apt-get install python-gst0.10")
     sys.exit(1)
 
 UP_LEFT = 0
@@ -27,23 +30,40 @@ UP_RIGHT = 1
 DOWN_LEFT = 2
 DOWN_RIGHT = 3
 
+class GstPlayer(object):
+    def __init__(self, player="player"):
+        self.player = gst.element_factory_make("playbin", player)
+        self.bus = self.player.get_bus()
+    def play(self, song):
+        if not os.path.exists(song):
+            sys.stderr.write("song path does not exists: " + song)
+            return
+        fileuri = "file://" + urllib.pathname2url(song)
+        if not gst.uri_is_valid(fileuri):
+            sys.stderr.write("song uri is not valid: " + fileuri)
+            return
+        self.player.set_property("uri", fileuri)
+        self.player.set_state(gst.STATE_PLAYING)
+    def stop(self):
+        self.player.set_state(gst.STATE_NULL)
+    def volume(self, volume):
+        self.player.set_property('volume', volume)
+
 class DynamicSound(object):
     def __init__(self):
-        self._sound = [None] * 4
-        self._channel = [None] * 4
+        self._players = [GstPlayer("player%i"%i) for i in xrange(4)]
         self._capture = cv.CaptureFromCAM(-1)
-        pygame.mixer.init(channels=2) # 1 <= channels <= 2
         self.capturing = False
         self._weight = [0.0] * 4
         self.cone = None
     def __del__(self):
-        pygame.mixer.fadeout(800)
-        # close mixer
-        pygame.mixer.quit()
+        # stop playing
+        for player in self._players:
+            player.stop()
 
     def setvolume(self, volume):
         for i in xrange(4):
-            self._channel[i].set_volume(volume[i])
+            self._players[i].volume(volume[i])
         print(self)
 
     def play(self, sounds):
@@ -52,11 +72,8 @@ class DynamicSound(object):
                        soundupleft, soundupright, sounddownleft, sounddownright
         """
         for i in xrange(4):
-            self._sound[i] = pygame.mixer.Sound(sounds[i])
-        print("debug: 4 sounds loaded")
-        for i in xrange(4):
-            self._channel[i] = self._sound[i].play()
-            self._channel[i].set_volume(0.1)
+            self._players[i].play(sounds[i])
+            self._players[i].volume(0.1)
         print("debug: 4 sounds playing")
 
     def capture(self):
@@ -128,7 +145,7 @@ class DynamicSound(object):
     def sum_to_weight(self, sums):
         maxsum = max(sums)
         posmax = sums.index(maxsum)
-        self._weight[posmax] += maxsum / sum(sums)
+        self._weight[posmax] += 1.0 # maxsum / sum(sums)
         self._weight = [w / 2.0 for w in self._weight]
 
     def image_to_weight(self, image):
